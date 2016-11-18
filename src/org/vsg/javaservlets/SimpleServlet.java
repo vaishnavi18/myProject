aa9ae098cd68c648b604fa9532243b85fed905ff
User <User@User-PC>
2016-11-18 20:58:57 +0530

    new

---
+++ src/org/vsg/javaservlets/SimpleServlet.java
@@ -0,0 +1,34 @@
+package org.vsg.javaservlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(
+		description = "A simple servlet by vaishnavi", 
+		urlPatterns = { "/SimpleServlet" }, 
+		initParams = { 
+				@WebInitParam(name = "a", value = "1", description = "first variable"), 
+				@WebInitParam(name = "b", value = "2", description = "second variable")
+		})
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Hello from simple servlet.");
+	}
+
+	
+}
